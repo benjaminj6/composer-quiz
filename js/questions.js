1160addf5efb61e86679957a5bb2869eb91bdbbe
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-07-15 01:20:03 -0700

    created easy quiz object

--- js/questions.js
+++ js/questions.js
@@ -35,17 +35,58 @@ var quiz = {
 		newQuestion: function(questionNumber) {
 			var propertyName = 'question' + questionNumber;
 			this[propertyName] = Object.create(quiz.question);
-			this[propertyName].questionNumber = questionNumber
+			this[propertyName].questionNumber = questionNumber;
 		},
 };
 
-quiz.newQuestion(3);
-
-
+console.log(quiz.newQuestion(3));
 
 //easy = new quiz
 var easy = Object.create(quiz);
 easy.newQuestion(1);
+//question1 properties
+easy.question1.question = 'The 1984 film \'Amadeus\' was based off of the ' +
+	'life of this famous composer.';
+easy.question1.correctAnswer = 'Wolfgang Amadeus Mozart';
+easy.question1.incorrectAnswers = ['John Cage', 'Karl Amadeus Hartmann', 'Igor Stravinsky'];
+easy.question1.answerInfo = '';
+easy.question1.answerPicture = '';
+//question2 properties
+easy.newQuestion(2);
+easy.question2.question = 'Which composer wrote the film scores for \'Star Wars\', ' + 
+	' \'Jurassic Park\', and \'Indiana Jones\'?';
+easy.question2.correctAnswer = 'John Williams';
+easy.question2.incorrectAnswers = ['Hans Zimmer', 'Howard Shore', 'Michael Giacchino'];
+easy.question2.answerInfo = '';
+easy.question2.answerPicture = '';
+//question3 properties
+easy.newQuestion(3);
+easy.question3.question = 'Which famous composer was deaf?';
+easy.question3.correctAnswer = 'Ludwig Von Beethoven';
+easy.question3.incorrectAnswers = ['Wolfgang Amadeus Mozart', 'Johann Sebastian Bach', 'Frederic Chopin'];
+easy.question3.answerInfo = '';
+easy.question3.answerPicture = '';
+//question4 properties
+easy.newQuestion(4);
+easy.question4.question = 'Which composer is best known for his ballets, the most famous ' + 
+	' ones being \'The Nutcracker\' and \'Swan Lake\'?';
+easy.question4.correctAnswer = 'Pyotr Ilych Tchaikovsky';
+easy.question4.incorrectAnswers = ['Joseph Haydn', 'Edward Elgar', 'Franz Liszt'];
+easy.question4.answerInfo = '';
+easy.question4.answerPicture = '';
+//question5 properties
+easy.newQuestion(5);
+easy.question5.question = 'Which composer is well known for his \'Hallelujah Chorus\'? (Hint: it\'s ' +
+	'performed a lot at Christmastime)';
+easy.question5.correctAnswer = 'George Frideric Handel';
+easy.question5.incorrectAnswers = ['Sergei Rachmaninov', 'Gustav Mahler', 'Felix Mendelssohn'];
+easy.question5.answerInfo = '';
+easy.question5.answerPicture = '';
+
+
+
+console.log(easy);
+
 
 
 	//questions
