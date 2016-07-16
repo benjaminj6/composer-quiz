0185d4064ff23fab240211819d1322805384d826
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-07-16 00:29:58 -0700

    quiz answer info added to easy quiz object

--- js/questions.js
+++ js/questions.js
@@ -3,7 +3,7 @@
 //easyQuiz
 
 var easyQuestions = [
-	{
+	{ //question1
 		question: 'The 1984 film \'Amadeus\' was based off of the life of which famous composer?',
 		correctAnswer: 'Wolfgang Amadeus Mozart',
 		incorrectAnswers: ['John Cage', 'Karl Amadeus Hartmann', 'Igor Stravinsky'],
@@ -11,42 +11,42 @@ var easyQuestions = [
 		answerPicture: 'images/mozart.jpg',
 	},
 	
-	{
+	{ //question2
 		question: 'Which composer wrote the film scores for \'Star Wars\', \'Jurassic Park\', and \'Indiana Jones\'?',
 		correctAnswer: 'John Williams',
 		incorrectAnswers: ['Hans Zimmer', 'Howard Shore', 'Michael Giacchino'],
-		answerInfo: '		John Williams is one of film\'s most prolific composers - he has won 5 Academy Awards, 4 Golden Globe Awards, 7 British Academy Film Awards, and 22 Grammy Awards (not to mention holding the record for the most award nominations held by a living person). Over his 60 years in the film industry he has composed over 130 scores, in addition to over 40 classical compositions.',
+		answerInfo: '		John Williams (1932 - present) is currently one of film\'s most prolific composers - he has won 5 Academy Awards, 4 Golden Globe Awards, 7 British Academy Film Awards, and 22 Grammy Awards (not to mention holding the record for the most award nominations held by a living person). Over his 60 years in the film industry he has composed over 130 scores, in addition to over 40 classical compositions.',
 		answerPicture: 'images/williams.jpg',
 	},
 
-	{
+	{ //question3
 		question: 'Which composer is well known for his ballets, the most famous ones being \'The Nutcracker\' and \'Swan Lake\'?',
 		correctAnswer: 'Pyotr Ilych Tchaikovsky',
 		incorrectAnswers: ['Joseph Haydn', 'Edward Elgar', 'Franz Liszt'],
-		answerInfo: '',
+		answerInfo: 'Pyotr Ilych Tchaikovsky (1840-1893) was one of the first Russian composers to be recognized internationally. His pieces are known for creating a classical sound that was also distinctly \'Russian\'. Although Tchaikovsky\'s music was well recieved by the Russian public, he suffered from depression. It is actually debated whether his death at age 53 was from cholera or self-inflicted.',
 		answerPicture: 'images/tchaikovsky.jpg',
 	},
 	
-	{
+	{ //question4
 		question: 'Which famous composer was deaf?',
-		correctAnswer: 'Ludwig Von Beethoven',
-		incorrectAnswers: ['Wolfgang Amadeus Mozart', 'Johann Sebastian Bach', 'Frederic Chopin'],
-		answerInfo: '',
+		correctAnswer: 'Ludwig Van Beethoven',
+		incorrectAnswers: ['Dmitri Shostakovich', 'Johann Sebastian Bach', 'Frederic Chopin'],
+		answerInfo: 'Ludwig Van Beethoven (1770-1827) is considered widely as one of the most influential figures in the Western Music tradition. The German composer began losing his hearing around age 30, and by the age of 44 he was almost completely deaf. It is even said that at the premier of his Ninth Symphony in 1824 he had to be turned around to see the audience\'s applause because he was unable to hear it.',
 		answerPicture: 'images/beethoven.jpg',
 	},
 
-	{
-		question: 'Which composer is best known for his \'Hallelujah Chorus\'? (Hint: it\'s most often performed at Christmastime)',
+	{ //question5
+		question: 'Which composer is best known for writing the \'Hallelujah Chorus\'? (Hint: it\'s most often performed at Christmastime)',
 		correctAnswer: 'George Frideric Handel',
 		incorrectAnswers: ['Sergei Rachmaninov', 'Gustav Mahler', 'Felix Medlessohn'],
-		answerInfo: '',
+		answerInfo: 'The \'Hallelujah Chorus\' was written by George Frideric Handel as part of an oratorio called \'The Messiah\'. An oratorio is a large composition written for an orchestra, choir, and soloists. Although Handel originally wrote \'The Messiah\' for a smaller orchestra and choir, modern productions have been known to perform the piece with 300-piece orchestras and 500-voice choirs (or larger!)',
 		answerPicture: 'images/handel.jpg',
 	},		
 ];
 
 var hardQuestions = [
 
-	{
+	{ //question1
 		question: 'What was Hildegard von Bingen\'s favorite color?',
 		correctAnswer: 'Green',
 		incorrectAnswers: ['Red', 'Yellow', 'Blue'],
@@ -54,15 +54,15 @@ var hardQuestions = [
 		answerPicture: 'images/hildegard.jpg',
 	},
 
-	{
-		question: 'Which composer was an obsessive perfectionist that he regularly synchronized all of the clocks in his apartment?',
+	{ //question2
+		question: 'Which composer was such an obsessive perfectionist that he regularly synchronized all of the clocks in his apartment?',
 		correctAnswer: 'Dmitri Shostakovich',
-		incorrectAnswers: ['Antonio Vivaldi, Ludwig von Beethoven', 'Gustav Mahler'],
+		incorrectAnswers: ['Antonio Vivaldi, Ludwig Van Beethoven', 'Gustav Mahler'],
 		answerInfo: '',
 		answerPicture: 'images/shostakovich.jpg',
 	},
 
-	{
+	{ //question3
 		question: 'Which composer is well known for his piece 4\'33", in which the performer sits siilently for 4 minutes and 33 seconds?',
 		correctAnswer: 'John Cage',
 		incorrectAnswers: ['Anton Webern', 'Arnold Schoenberg', 'Edward Elgar'],
@@ -70,7 +70,7 @@ var hardQuestions = [
 		answerPicture: 'images/cage.jpg',
 	},
 
-	{
+	{ //question4
 		question: 'Which composer was the father of 20 children?',
 		correctAnswer: 'Johann Sebastian Bach',
 		incorrectAnswers: ['George Gershwin'. 'Gustav Mahler', 'Johann Strauss'],
@@ -78,7 +78,7 @@ var hardQuestions = [
 		answerPicture: 'images/bach.jpg',
 	},
 
-	{
+	{ //question5
 		question: 'Which composer died of a chill after coming home from the pub one night only to find that his wife had locked him out?',
 		correctAnswer: 'Henry Purcell',
 		incorrectAnswers: ['Pyotr Ilych Tchaikovsky', 'Frederic Chopin', 'Hector Berlioz'],
