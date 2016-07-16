8c8a1c2c5b7bfd8e8378680bcf6c0975cac7e1f8
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-07-16 11:39:12 -0700

    js functions outlining, removed css active class and replaced with an inactive class

--- js/app.js
+++ js/app.js
@@ -1,53 +1,62 @@
 $(document).ready(function() {
 
+/*--- PURE FUNCTIONS ---*/
 
+//crawls through index of chosen array - when done returns to 0
+var index = (function() {
+	var cnt = 0;
+
+	return function() {
+		if (cnt < 4) {
+			return cnt++;
+		} else {
+			return cnt = 0;	//sets the limit at 0 before counter resets.
+		};
+	}
+}());
+
+//increases and stores the current score value
+var index = (function() {
+	var score = 0;
+
+	return function() {
+		if(score < 5) {
+			return ++score;
+		} else {
+			return score = 0;
+		}
+	}
+}());
+
+/*--- GLOBAL VARIABLES ---*/
+//var quiz = selectQuiz() return value;
+
+var quiz;
+/*--- FUNCTIONS ---*/
+//function selectQuiz(selection)
+//displayQuestion(currentQuestion)
+//changeScreen(current, next)
+
+quiz = easy;
+console.log(quiz);
+quiz = hard;
+console.log(quiz);
 /*--- EVENT HANDLERS ---*/
+// NEW-GAME SCREEN
+	//difficulty-buttons hover toggle hover
+	//difficulty-buttons click toggle selected
+		//start-quiz click toggle active
+	//start-quiz click 
+		//find the selected quiz and sets global variable quiz equal to that.
+		//displayQuestion()	
 
-//opening screen
-
-//easy/hard button hover
-
-	//easy/hard button select
-		//adds selected class
-		//loads correct game
-		//activates start game button
-	//start game button hover
-	//start game click
-		//hides opening-screen and displays question screen
-		//display question info
-
-//quiz question screen
-	//user hovers over a list item
-	//user selects a list item
-		//class selected on chosen item
-		//activates submit answer button
-	//user hovers on submit answer button
-	//user clicks submit answer button
-		//evaluates the selection and the correct answer
-			//if true then adds one to # correct
-		//hides the questions screen and displays the answers 
-		//populates the new screen display answer function
-		//if answer is the last question in the quiz then changes button text
-			//to 'See Your Score >>'
-				//add 'last button' class?
-			
-
-//answer screen
-	//user hovers on next question button
-	//user clicks next question button
-		//moves to the next question in the quiz
-		//hides answer screen and display quiz screen
-	//user clicks 'see your score' button
-		//hides current scree and shows the new game screen
-		//populates the new game screen with the appropriate information
-
-//end game 'screen' 
+// QUESTIONS-SCREEN
+	//
+// ANSWERS-SCREEN
+// END-GAME SCREEN
 
 
-/*--- GENERAL FUNCTIONS ---*/
-//hover
-//user selection
-//button activate
-//newquestion - takes the question number as a parameter
+
+
 
 });
